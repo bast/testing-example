5ba2605ae510dc7f807f7471e8de9903ad4d3ccc
Radovan Bast <bast@users.noreply.github.com>
2023-09-28 09:50:28 +0200

    added few tests, let's see

--- example.py
+++ example.py
@@ -1,8 +1,19 @@
 def add(a, b):
     return a + b
 
+def test_add():
+    assert add(2, 3) == 5
+
+
 def subtract(a, b):
     return a + b
+    
+def test_subtract():
+    assert subtract(2, 3) == -1
+
 
 def multiply(a, b):
     return a * b
+    
+def test_multiply():
+    assert multiply(2, 3) == 6
